62e03c26398efbbe0e26ab038b350b1236cb695c
hubert <hubert@hubert.com>
2024-06-11 14:28:06 +0200

    first

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::ffi::OsStr;
-use std::ffi::OsString;
-use std::path::PathBuf;
+use std::{fs::File, io::Write};
 
-use clap::{Args, Parser, Subcommand, ValueEnum};
+use askama::Template;
+use clap::{Parser, Subcommand};
 
 /// A fictional versioning CLI
 #[derive(Debug, Parser)] // requires `derive` feature
@@ -18,7 +17,29 @@ enum Commands {
     ///Initialize Nix repository
     Init,
 }
+#[derive(Template)]
+#[template(path = "build.nix.j2")]
+struct Build {}
 
-fn main() {
+#[derive(Template)]
+#[template(path = "default.nix.j2")]
+struct Default {}
+
+#[derive(Template)]
+#[template(path = "shell.nix.j2")]
+struct Shell {}
+
+fn main() -> std::io::Result<()> {
     let args = Cli::parse();
+    match args.command {
+        Commands::Init => {
+            let mut build_nix = File::create("build.nix")?;
+            let mut shell_nix = File::create("shell.nix")?;
+            let mut default_nix = File::create("default.nix")?;
+            build_nix.write_all(Build {}.render().unwrap().as_bytes())?;
+            shell_nix.write_all(Default {}.render().unwrap().as_bytes())?;
+            default_nix.write_all(Shell {}.render().unwrap().as_bytes())?;
+        }
+    }
+    Ok(())
 }
