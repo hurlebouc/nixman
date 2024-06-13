39cc191180a8f177f0b6020295431c3673cb777b
hubert <hubert@hubert.com>
2024-06-13 06:29:46 +0200

    rust-src in nix shell

--- default.nix
+++ default.nix
@@ -1,14 +1,16 @@
- let
-   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
-   pkgs = import nixpkgs { config = {}; overlays = []; };
-   build = pkgs.callPackage ./build.nix {};
- in
- {
-   build = build;
-   shell = pkgs.mkShellNoCC {
-     inputsFrom = [ build ];
-     packages = [
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
+  build = pkgs.callPackage ./build.nix { };
+in
+{
+  build = build;
+  shell = pkgs.mkShellNoCC {
+    #RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+    inputsFrom = [ build ];
+    packages = [
       pkgs.nixpkgs-fmt
-     ];
-   };
- }
+    ];
+  };
+}
